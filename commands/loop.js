db8ff43508201d597898aa5f4b968f278b7eae0a
RubenCipriano <57687107+RubenCipriano@users.noreply.github.com>
2019-12-15 00:07:20 +0000

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -2,12 +2,12 @@ module.exports.run = async(bot,message,args,ops) => {
     if(ops.loop == false)
     {
         ops.loop = true;
-        return message.channel.send(":white_check_mark: Foi ativado o Loop!");
+        return message.channel.send(":white_check_mark: Foi ativado o Loop!").then(msg => msg.delete(10000)).catch;;
     }
     if(ops.loop == true)
     {
         ops.loop = false;
-        return message.channel.send("❌ Foi desativado o Loop!");
+        return message.channel.send("❌ Foi desativado o Loop!").then(msg => msg.delete(10000)).catch;;
     }
 }
 
