42afac065c94f5fd490b130f17a6771fc8f643c9
RubenCipriano <57687107+RubenCipriano@users.noreply.github.com>
2019-12-19 15:33:03 +0000

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -1,10 +1,10 @@
-const active = new Map();
 module.exports.run = async(bot,message,args,config) => {
     if(!message.member.voiceChannel) return message.channel.send('Por favor entre em uma sala de audio!').then(msg => msg.delete(10000)).catch();
     if(!message.guild.me.voiceChannel) return message.channel.send('O Bot não está conectado a um canal de audio!').then(msg => msg.delete(10000)).catch();
     if(message.guild.me.voiceChannel.id != message.member.voiceChannelID) return message.channel.send('O bot e tu não se encontram no mesmo canal de audio!').then(msg => msg.delete(10000)).catch();
-    config.active = active;
-    message.guild.me.voiceChannel.leave();
+    config.active.delete(config.active.guildID);
+    let vc = bot.guilds.get(dispatcher.guildID).me.voiceChannel;
+    if(vc) vc.leave();
     message.channel.send('Sai do Canal de Voz, Obrigado!').then(msg => msg.delete(10000)).catch();
 }
 
