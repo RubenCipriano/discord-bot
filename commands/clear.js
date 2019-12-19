b6e18798e7f374b95811d2c424f720002b48c121
RubenCipriano <57687107+RubenCipriano@users.noreply.github.com>
2019-12-19 15:36:37 +0000

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,7 +3,7 @@ module.exports.run = async(bot,message,args,config) => {
       if (message.member.hasPermission("MANAGE_MESSAGES")) {
         message.channel.fetchMessages()
           .then(function(list){
-                message.channel.bulkDelete(list).catch;
+                message.channel.bulkDelete(list).catch();
             }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})                        
     }
 }
