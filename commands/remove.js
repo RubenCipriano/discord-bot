86399950d4f6f354f12cfbdc1a0acaef70dc737d
RubenCipriano <57687107+RubenCipriano@users.noreply.github.com>
2019-12-19 12:40:03 +0000

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -1,12 +1,19 @@
 module.exports.run = async(bot,message,args,config) => {
-      if(args[0] != undefined && args[0] >= 0)
+      try
       {
-            let fetched = config.active.get(message.guild.id);
-            let songTitle = fetched.queue[args[0]].songTitle;
-            fetched.queue.splice(args[0]);
-            return message.channel.send("Removemos: " + songTitle + " da queue!").then(msg => msg.delete(10000)).catch;;
+            if(args[0] != undefined && args[0] >= 0)
+            {
+                  let fetched = config.active.get(message.guild.id);
+                  let songTitle = fetched.queue[args[0]].songTitle;
+                  fetched.queue.splice(args[0]);
+                  return message.channel.send("Removemos: " + songTitle + " da queue!").then(msg => msg.delete(10000)).catch;;
+            }
+            return message.channel.send("Enviaste um valor não válido!").then(msg => msg.delete(10000)).catch;;
       }
-      return message.channel.send("Enviaste um valor não válido!").then(msg => msg.delete(10000)).catch;;
+      catch
+      {
+            return;     
+      }
 }
 
 module.exports.help = {
