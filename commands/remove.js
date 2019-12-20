12858cfaa019dad69592f89279492ecd077d0223
RubenCipriano <57687107+RubenCipriano@users.noreply.github.com>
2019-12-20 14:31:41 +0000

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -1,21 +1,23 @@
 module.exports.run = async(bot,message,args,config) => {
-      try
-      {
-            if(args[0] != undefined && args[0] >= 0)
-            {
-                  let fetched = config.active.get(message.guild.id);
-                  let songTitle = fetched.queue[args[0]].songTitle;
-                  fetched.queue.splice(args[0]);
-                  return message.channel.send("Removemos: " + songTitle + " da queue!").then(msg => msg.delete(10000)).catch;;
-            }
-            return message.channel.send("Enviaste um valor não válido!").then(msg => msg.delete(10000)).catch;;
-      }
-      catch
-      {
-            return;     
-      }
+    try
+    {
+          if(args[0] != undefined && args[0] >= 0)
+          {
+                let fetched = config.active.get(message.guild.id);
+                let songTitle = fetched.queue[args[0]].songTitle;
+                fetched.queue.splice(args[0]);
+                return message.channel.send("Removemos: " + songTitle + " da queue!").then(msg => msg.delete(10000)).catch;;
+          }
+          return message.channel.send("Enviaste um valor não válido!").then(msg => msg.delete(10000)).catch;;
+    }
+    catch
+    {
+          return;     
+    }
 }
 
 module.exports.help = {
-  name: 'remove',
+name: 'remove',
+description:'Remove uma musica da queue',
+usage:'!remove <Numero>'
 };
