12a96e1f8ad834944470480c09ee578b6dfabc07
RubenCipriano <57687107+RubenCipriano@users.noreply.github.com>
2019-12-22 20:18:35 +0000

    Update loopqueue.js

--- commands/loopqueue.js
+++ commands/loopqueue.js
@@ -2,12 +2,12 @@ module.exports.run = async(bot,message,args,ops) => {
     if(ops.loopqueue == false)
     {
         ops.loopqueue = true;
-        return message.channel.send(":white_check_mark: Foi ativado o Loop!");
+        return message.channel.send(":white_check_mark: Foi ativado o Loop da Queue!").then(msg => msg.delete(1000)).catch();
     }
     if(ops.loopqueue == true)
     {
         ops.loopqueue = false;
-        return message.channel.send("❌ Foi desativado o Loop!");
+        return message.channel.send("❌ Foi desativado o Loop da Queue!").then(msg => msg.delete(1000)).catch();
     }
 }
 
