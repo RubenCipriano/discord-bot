3521f91dd560478f08639a353b9fcbc923e79ae4
RubenCipriano <57687107+RubenCipriano@users.noreply.github.com>
2020-01-01 02:12:45 +0000

    Update loopqueue.js

--- commands/loopqueue.js
+++ commands/loopqueue.js
@@ -1,4 +1,5 @@
 module.exports.run = async(bot,message,args,ops) => {
+    if(!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(`Não tens permissão para fazer isto!`).then(msg => msg.delete(5000)).catch();
     let fetched = ops.active.get(message.guild.id);
     if(!fetched) return message.channel.send('No momento não existem musicas a tocar!').then(msg => msg.delete(10000)).catch;
     if(!fetched.loop || fetched.loop == false){
