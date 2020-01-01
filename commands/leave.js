d96acb09a6816ac7e9ec7baf9a1d5b36f9ca780e
RubenCipriano <57687107+RubenCipriano@users.noreply.github.com>
2020-01-01 02:12:21 +0000

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -1,4 +1,5 @@
 module.exports.run = async(bot,message,args,config) => {
+  if(!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(`Não tens permissão para fazer isto!`).then(msg => msg.delete(5000)).catch();
   if(!message.member.voiceChannel) return message.channel.send('Por favor entre em uma sala de audio!').then(msg => msg.delete(10000)).catch();
   if(!message.guild.me.voiceChannel) return message.channel.send('O Bot não está conectado a um canal de audio!').then(msg => msg.delete(10000)).catch();
   if(message.guild.me.voiceChannel.id != message.member.voiceChannelID) return message.channel.send('O bot e tu não se encontram no mesmo canal de audio!').then(msg => msg.delete(10000)).catch();
