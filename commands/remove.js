6246eac03699e08556c66bcceb9c23a87ea6f978
RubenCipriano <57687107+RubenCipriano@users.noreply.github.com>
2020-01-25 21:16:58 +0000

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -9,12 +9,10 @@ module.exports.run = async(bot,message,args,config) => {
       }
       else
       {
-          config.active.delete(config.active.guildID);
-          let vc = message.guild.me.voiceChannel;
-          if(vc) vc.leave();
-          for(var i = 0; i < fetched.queue.lenght; i++) {
-                fetched.queue.splice(0);
-           }
+           config.active.delete(config.active.guildID);
+           let vc = message.guild.me.voiceChannel;
+           if(vc) vc.leave();
+           fetched.queue = undefined;
            return message.channel.send("Removemos a Playlist Toda!").then(msg=> msg.delete(10000)).catch;
       }
 }
