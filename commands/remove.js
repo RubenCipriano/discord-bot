65ae47c134301278e8ed31d3d4333d04eacf5689
RubenCipriano <57687107+RubenCipriano@users.noreply.github.com>
2020-01-25 20:56:01 +0000

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -1,16 +1,17 @@
 module.exports.run = async(bot,message,args,config) => {
     if(!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(`Não tens permissão para fazer isto!`).then(msg => msg.delete(5000)).catch();
-    let fetched = config.active.get(message.guild.id);
       if(args[0] != undefined)
       {
+            let fetched = config.active.get(message.guild.id);
             let songTitle = fetched.queue[args[0]].songTitle;
             fetched.queue.splice(args[0]);
             return message.channel.send("Removemos: " + songTitle + " da queue!").then(msg => msg.delete(10000)).catch;;
       }
       else
       {
-            for(var i = 0; i < feched.queue.lenght - 1; i++) {
-                fetched.queue.splice(i);   
+            for(var i = 0; i < feched.queue.lenght; i++) {
+                let fetched = config.active.get(message.guild.id);
+                fetched.queue.splice(0);   
             }
           return message.channel.send("Removemos a Playlist Toda!").then(msg=> msg.delete(10000)).catch;
       }
