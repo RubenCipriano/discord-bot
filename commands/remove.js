da99cdbb336d27f59e663f1972d5cb39afa9c6a2
RubenCipriano <57687107+RubenCipriano@users.noreply.github.com>
2020-01-25 21:42:13 +0000

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -12,7 +12,7 @@ module.exports.run = async(bot,message,args,config) => {
           config.active.delete(config.active.guildID);
            let vc = message.guild.me.voiceChannel;
            if(vc) vc.leave();
-           for(var i = 1; i < fetched.queue.lenght;i++)
+           for(var i = 0; i < fetched.queue.lenght;i++)
                fetched.queue.splice(0);
            config.active.set(message.guildID,fetched);
            return message.channel.send("Removemos a Playlist Toda!").then(msg=> msg.delete(10000)).catch;
